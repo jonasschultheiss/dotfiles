6bc98e4fbf11ab92f30ed654e7f9ddae7760ace7
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2024-03-01 09:36:57 +0100

    chore: comment mas apps as no apple id at work

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -86,16 +86,16 @@
       "heroku/brew"
       "homebrew/cask-fonts"
     ];
-    masApps = {
-      # "Fantastical - Calendar & Tasks" = 975937182;
-      "Magnet" = 441258766;
-      "Microsoft Excel" = 462058435;
-      "Microsoft PowerPoint" = 462062816;
-      "Microsoft Word" = 462054704;
-      "PDF Expert: PDF bearbeiten" = 1055273043;
-      "Postico" = 1031280567;
-      "Slack for Desktop" = 803453959;
-      "Xcode" = 497799835;
-    };
+    # masApps = {
+    #   # "Fantastical - Calendar & Tasks" = 975937182;
+    #   "Magnet" = 441258766;
+    #   "Microsoft Excel" = 462058435;
+    #   "Microsoft PowerPoint" = 462062816;
+    #   "Microsoft Word" = 462054704;
+    #   "PDF Expert: PDF bearbeiten" = 1055273043;
+    #   "Postico" = 1031280567;
+    #   "Slack for Desktop" = 803453959;
+    #   "Xcode" = 497799835;
+    # };
   };
 }
