3c5718462be9cadb03133dab8aea13f13c697162
Jonas Schultheiss <complaint@jonasschultheiss.dev>
2025-03-10 15:01:52 +0100

    Simplify flake.nix for initial transition

--- flake.nix
+++ flake.nix
@@ -1,78 +1,87 @@
 {
-  description = "Multi-user nix-darwin configuration with flakes";
+  description = "Minimal nix-darwin configuration with flakes";
 
   inputs = {
-    # Package sources
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
-    # Environment/system management
-    darwin = {
-      url = "github:lnl7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs @ {
+  outputs = {
     self,
     nixpkgs,
     darwin,
     home-manager,
     ...
-  }: let
-    # System architecture
-    system = "aarch64-darwin"; # Change to x86_64-darwin if you're on Intel
-
-    # Configure nixpkgs
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        allowUnfree = true;
-      };
-    };
-
-    # Define user configurations
-    users = {
-      jonasschultheiss = {
-        username = "jonasschultheiss";
-        homeDirectory = "/Users/jonasschultheiss";
-      };
-      verastalder = {
-        username = "verastalder";
-        homeDirectory = "/Users/verastalder";
-      };
-    };
+  } @ inputs: let
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
-    darwinConfigurations.system = darwin.lib.darwinSystem {
+    darwinConfigurations.minimal = darwin.lib.darwinSystem {
       inherit system;
       modules = [
-        # nix-darwin base configuration
-        ./modules/darwin
+        # Basic darwin configuration
+        {
+          # Enable nix command and flakes
+          nix.settings.experimental-features = ["nix-command" "flakes"];
 
-        # Make home-manager work with nix-darwin
+          # Enable auto-optimise-store
+          nix.optimise.automatic = true;
+
+          # Allow unfree packages
+          nixpkgs.config.allowUnfree = true;
+
+          # Enable fish shell
+          programs.fish.enable = true;
+          environment.shells = with pkgs; [fish];
+
+          # Set hostname and system version
+          networking.hostName = "jonasschultheiss-mac";
+          system.stateVersion = 4;
+
+          # Add basic system packages
+          environment.systemPackages = with pkgs; [
+            alejandra
+            git
+          ];
+        }
+
+        # Add home-manager
         home-manager.darwinModules.home-manager
         {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users = {
-              # Import user-specific configurations
-              ${users.jonasschultheiss.username} = import ./modules/home/jonasschultheiss;
-              ${users.verastalder.username} = import ./modules/home/verastalder;
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          # Configure jonasschultheiss user
+          home-manager.users.jonasschultheiss = {pkgs, ...}: {
+            home.username = "jonasschultheiss";
+            home.homeDirectory = "/Users/jonasschultheiss";
+            home.stateVersion = "22.05";
+
+            # Enable home-manager
+            programs.home-manager.enable = true;
+
+            # Enable fish
+            programs.fish.enable = true;
+
+            # Enable git
+            programs.git = {
+              enable = true;
+              userName = "Jonas Schultheiss";
+              userEmail = "jonas.schultheiss@example.com";
             };
+
+            # Basic packages
+            home.packages = with pkgs; [
+              fd
+              eza
+              bat
+              ripgrep
+            ];
           };
         }
-      ];
-    };
-
-    # Convenient development shell for bootstrapping
-    devShell.${system} = pkgs.mkShell {
-      buildInputs = [
-        pkgs.nixfmt
       ];
     };
   };
